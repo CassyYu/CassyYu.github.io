4865f15e5becaa4f2f53f5e8f77966cc6b3753ed
CassyYu <980317866@qq.com>
2021-05-18 16:54:05 +0800

    questions about state: this.props

--- src/Components/Right/index.jsx
+++ src/Components/Right/index.jsx
@@ -7,29 +7,22 @@ export default class Right extends Component {
 
   state = {
     timerId: null,
-    currentChoosenId: -1,
+    currentChoosenId: this.props.currentChoosenID
   }
 
   handleClickStart = () => {
-    const { choices } = this.props
-    const { length } = choices
+    const { choices } = this.props;
+    const { length } = choices;
+    if (this.state.timerId !== null) return;
     if (length === 0) {
       alert('无选项');
       return;
     }
-    if (this.state.timerId !== null) {
-      return;
-    }
     let t = 0;
     const timerId = setInterval(() => {
-      this.setState({
-        currentChoosenId: t++ % choices.length,
-      });
+      this.setState({ currentChoosenId: t++ % choices.length });
     }, TIMER_INTERVAL);
-    this.setState({
-      timerId,
-    });
-    console.log(1)
+    this.setState({ timerId });
   }
 
   handleClickStop = () => {
@@ -38,15 +31,16 @@ export default class Right extends Component {
   }
 
   renderCurrent() {
-    const { choices } = this.props;
+    const { choices, currentChoosenID } = this.props;
     const { currentChoosenId } = this.state;
+    console.log('id:' + currentChoosenId)
     if (choices.length === 0 || currentChoosenId === -1) {
       return (
         <div className="final-choice" style={{ backgroundColor: '#ddd' }}>
           <div className="final-choice-text">{'点击Start开始'}</div>
         </div>);
     }
-    const { value: text, color } = choices[currentChoosenId];
+    const { text, color } = choices[currentChoosenId];
     return (
       <div className="final-choice" style={{ backgroundColor: color }}>
         {<div className="final-choice-text">{text}</div>}
@@ -54,6 +48,7 @@ export default class Right extends Component {
   }
 
   render() {
+    console.log('renderRight');
     return (
       <div className="right">
         <h2 className="right-header">Your Final Choice</h2>
